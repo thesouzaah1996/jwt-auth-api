172093834f1b808bf1d6893c07bd1894c755a0fa
Matheus <matheus.de.souza.code@gmail.com>
2025-07-20 17:46:20 -0300

    first commit

---
+++ src/main/java/com/thesouzaah/jwt_auth_api/JwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.thesouzaah.jwt_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthApiApplication.class, args);
+	}
+
+}
